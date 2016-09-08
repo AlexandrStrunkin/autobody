cf882dbdb3aaa72da114c4794b95a8c596881a80
Admin <git@webgk.ru>
2016-09-08 15:07:58 +0300

    SUP-4920: fixing CSS styles for search title template

--- test/index.php
+++ test/index.php
@@ -1,4 +1,22 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");?>
 <?
-
+error_reporting(E_ALL);
+// получаем нужную информацию
+    $postdata = http_build_query(
+	    array(
+	       'token' => '11fdb14cdd8e77200bdade5841703560'
+	   )
+	);
+	
+	$opts = array('http' =>
+	   array(
+	       'method'  => 'POST',
+	       'header'  => 'Content-type: application/x-www-form-urlencoded',
+	       'content' => $postdata
+	  )
+	);
+	
+	$context  = stream_context_create($opts);
+	$result = file_get_contents('http://www.autobody.ru/api/getCatalog/', false, $context);
+	arshow($result);
 ?>
