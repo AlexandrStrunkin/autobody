187a2b0110010b84055d6affa4ca3502bfdad80e
Alexandr <as@webgk.ru>
2016-09-21 11:53:21 +0300

    SUP-4917: redisign popup

--- local/templates/.default/components/bitrix/catalog/main_catalog/bitrix/catalog.section/.default/component_epilog.php
+++ local/templates/.default/components/bitrix/catalog/main_catalog/bitrix/catalog.section/.default/component_epilog.php
@@ -3,9 +3,9 @@
 /* Скрипт вывода всплывающего окна на странице каталога */
     $(document).ready(function(){
         if ($.cookie('info_popup') != 'Y') {
-            $(".forward_catalog_new_info_popup").show(500);
+            $(".forward_catalog_new_info_popup").toggle(500);
             $("body").on("click", ".forward_catalog_new_popup_close_button",  function(){
-                $(".forward_catalog_new_info_popup").hide(500);
+                $(".forward_catalog_new_info_popup").toggle(500);
                 $.cookie('info_popup', 'Y', { expires: 365, path: '/'});
             });
         }
